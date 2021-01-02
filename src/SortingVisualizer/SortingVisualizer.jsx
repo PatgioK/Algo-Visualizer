1a728fb6cdd8006264f7083f8f0d924f6d42b273
Patgio <Patgiok@gmail.com>
2021-01-01 23:52:44 -0800

    mergesort and animations added, bug with 2nd half mergesort.

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import './SortingVisualizer.css';
+import { getMergeSortAnimations } from '../SortingAlgorithms/SortingAlgorithms';
+
+
+const PRIMARY_COLOR = 'aqua';
+
+const SECONDARY_COLOR = 'green';
+
+// Speed of the animation in ms.
+const ANIMATION_SPEED_MS = 3;
 
 export default class SortingVisualizer extends React.Component {
     constructor(props) {
@@ -21,6 +30,32 @@ export default class SortingVisualizer extends React.Component {
         this.setState({array});
     }
 
+    mergeSort() {
+        const animations = getMergeSortAnimations(this.state.array);
+        for(let i = 0; i < animations.length; i++){
+            const arrayBars = document.getElementsByClassName('array-bar');
+
+            // Animations come in triplets. First two animations are what we are comparing
+            const isColorChange = i % 3 !== 2;
+            if(isColorChange) {
+                const[barOneIdx, barTwoIdx] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED_MS);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i * ANIMATION_SPEED_MS);
+            }
+        }
+    }
+
 
     render() {
         const {array} = this.state;
@@ -39,8 +74,11 @@ export default class SortingVisualizer extends React.Component {
                     <div
                     className='array-bar' 
                     key={idx}
-                    // $ dollarsign makes a css variable
-                    style={{height: `${value}px`}}
+                    // $ dollarsign makes a css variable???
+                    style={{
+                        backgroundColor: PRIMARY_COLOR,
+                        height: `${value}px`,
+                    }}
                     >
                     </div>
                 ))}
